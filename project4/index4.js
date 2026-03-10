e538f628be2f898c375ddc5194052d6dad6f0d3a
Hamim <hamimsiddiq915@gmail.com>
2026-03-11 00:34:53 +0600

    project4

---
+++ project4/index4.js
@@ -0,0 +1,38 @@
+const randomNumber = parseInt(Math.random() * 100 + 1)
+
+const submit = document.querySelector("#guessSubmit")
+const userInput = document.querySelector("#guessField")
+const guessSlot = document.querySelector(".gusses")
+const remaining = document.querySelector(".remaining")
+const lowOrHi = document.querySelector(".lowOrHi")
+const resultPara = document.querySelector(".result")
+
+const p = document.createElement("p")
+
+let prevGuess = []
+let numGuess = 1
+let playGame = true
+
+function validateGuess(guess){
+
+}
+
+function checkGuess(guess){
+    
+}
+
+function displayGuess(guess){
+    
+}
+
+function displayMsg(msg){
+    
+}
+
+function endGame(){
+    
+}
+
+function newGame(){
+    
+}
