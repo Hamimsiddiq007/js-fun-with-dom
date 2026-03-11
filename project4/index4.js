c59467ed1bf28e3d7363b255c8e10b7c3f8940c8
Hamim <hamimsiddiq915@gmail.com>
2026-03-11 20:05:43 +0600

    guessing will continue

--- project4/index4.js
+++ project4/index4.js
@@ -13,20 +13,53 @@ let prevGuess = []
 let numGuess = 1
 let playGame = true
 
-function validateGuess(guess){
+if(playGame){
+    submit.addEventListener("click", (e) => {
+        e.preventDefault()
+        const guess = parseInt(userInput.value) 
+        validateGuess(guess)
+    })
+}
 
+function validateGuess(guess){
+    if(isNaN(guess)){
+        alert("Please enter a valid number")
+    } else if (guess < 1 || guess > 100){
+        alert("Please enter a number between 1 and 100")
+    } else {
+        prevGuess.push(guess)
+       if(numGuess === 11){
+            displayGuess(guess)
+            displayMsg(`Game over. Random number was ${randomNumber}`)
+            endGame()
+       } else{
+        displayGuess(guess)
+        checkGuess(guess)
+       }
+    }
 }
 
 function checkGuess(guess){
-    
+    if(guess ===randomNumber){
+        displayMsg(`You got it right!`)
+        endGame()
+    } else if (guess < randomNumber){
+        displayMsg(`Your guess is too low`)
+    } else if (guess > randomNumber){
+        displayMsg(`Your guess is too high`)
+    }
 }
 
 function displayGuess(guess){
-    
+    userInput.value = ""
+    guessSlot.innerHTML += `${guess}, `
+    numGuess++
+    remaining.innerHTML = `${11 - numGuess}`
 }
 
 function displayMsg(msg){
-    
+    lowOrHi.innerHTML = `<h3>${msg}</h3>`
+    resultPara.style.backgroundColor = "cadetblue"
 }
 
 function endGame(){
